74577561b6281d0472dd9ef2b0d7c4be0cb49416
Kainat <im493177@gmail.com>
2024-10-02 02:00:53 -0700

    First commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/my-project/",
+  base:"/ecommercewebsite/",
   plugins: [react()],
+  optimizeDeps:{
+    include:['react-simple-typewriter']
+  }
 })
